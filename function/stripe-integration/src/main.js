657a32edf33352c0fbe82d0a340ccd9a230d8aa9
Arielqqq <ariel.wu.94@gmail.com>
2023-11-01 21:45:48 +1100

    Stripe integration

--- function/stripe-integration/src/main.js
+++ function/stripe-integration/src/main.js
@@ -1,33 +1,23 @@
+const stripe = require("stripe")('sk_test_51O4hxbGdMUVbjnpKnGTGyDEn2z1G1Ft8KpzR4KooQ6FxO7W5XFZJWnsY4ejtpPw9OpJzdWtTkxm2rUcRqDEU41Ml00AWDzN5G0');
+
 import { Client } from 'node-appwrite';
 
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+  const { items, amount } = req.body;
 
-  // You can log messages to the console
-  log('Hello, Logs!');
-
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
-
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
-
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "aud",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
   });
-};
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+  };
